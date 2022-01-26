687a5efd38365247a38b4bf83f349bfce4532c43
summyabubakar <summyabubakar03@gmail.com>
2022-01-26 12:46:11 +0100

    homepage changed

--- db/hms.sql
+++ db/hms.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 12:22 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
@@ -34,18 +12,8 @@ CREATE TABLE `admin` (
   `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
-
 INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
 (1, 'admin', 'Test@12345', '28-12-2016 11:42:05 AM');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `appointment`
---
 
 CREATE TABLE `appointment` (
   `id` int(11) NOT NULL,
@@ -61,9 +29,6 @@ CREATE TABLE `appointment` (
   `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `appointment`
---
 
 INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
 (1, 'Dentist', 1, 1, 500, '2016-12-31', '09:25', '2017-01-01 00:29:02', 1, 0, ''),
@@ -72,11 +37,6 @@ INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `
 (4, 'Gynecologist/Obstetrician', 9, 8, 500, '2022-01-21', '10:30 AM', '2022-01-21 09:26:07', 0, 1, ''),
 (5, 'Gynecologist/Obstetrician', 10, 8, 600, '2022-01-21', '10:35 AM', '2022-01-21 09:29:59', 1, 1, '');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `doctors`
---
 
 CREATE TABLE `doctors` (
   `id` int(11) NOT NULL,
@@ -91,19 +51,10 @@ CREATE TABLE `doctors` (
   `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctors`
---
-
 INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
 (9, 'Gynecologist/Obstetrician', 'Sumayya', 'Abuja, Nigeria', '500', 8108385819, 'summyabubakar03@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2022-01-21 09:18:52', NULL),
 (10, 'Gynecologist/Obstetrician', 'Summy', 'Abuja Nigeria', '600', 8108385819, 'test@gmail.com', 'cc03e747a6afbbcbf8be7668acfebee5', '2022-01-21 09:29:03', NULL);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `doctorslog`
---
 
 CREATE TABLE `doctorslog` (
   `id` int(11) NOT NULL,
@@ -115,9 +66,6 @@ CREATE TABLE `doctorslog` (
   `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctorslog`
---
 
 INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
 (11, NULL, 'summyabubakar03@gmail.com', 0x3a3a3100000000000000000000000000, '2022-01-21 09:27:36', NULL, 0),
@@ -134,11 +82,6 @@ INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logou
 (22, NULL, 'summyabubakar03@gmail.com', 0x3a3a3100000000000000000000000000, '2022-01-21 10:54:18', NULL, 0),
 (23, 10, 'test@gmail.com', 0x3a3a3100000000000000000000000000, '2022-01-21 10:54:26', NULL, 1);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `doctorspecilization`
---
 
 CREATE TABLE `doctorspecilization` (
   `id` int(11) NOT NULL,
@@ -147,21 +90,11 @@ CREATE TABLE `doctorspecilization` (
   `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `doctorspecilization`
---
-
 INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
 (1, 'Gynecologist/Obstetrician', '2022-01-21 06:37:25', '2022-01-21 09:12:47'),
 (2, 'General Physician', '2022-01-20 06:38:12', '2022-01-21 09:13:15'),
 (3, 'Dermatologist', '2022-01-22 06:38:48', '2022-01-21 09:13:34'),
 (6, 'Dentist', '2022-01-20 06:40:08', '2022-01-21 09:13:51');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblcontactus`
---
 
 CREATE TABLE `tblcontactus` (
   `id` int(11) NOT NULL,
@@ -175,18 +108,8 @@ CREATE TABLE `tblcontactus` (
   `IsRead` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblcontactus`
---
-
 INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `PostingDate`, `AdminRemark`, `LastupdationDate`, `IsRead`) VALUES
 (1, 'test user', 'test@gmail.com', 2523523522523523, ' This is sample text for the test.', '2022-01-21 19:03:08', 'Test Admin Remark', '2022-01-21 10:25:23', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userlog`
---
 
 CREATE TABLE `userlog` (
   `id` int(11) NOT NULL,
@@ -198,19 +121,10 @@ CREATE TABLE `userlog` (
   `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `userlog`
---
 
 INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
 (16, 8, 'summyabubakar03@gmail.com', 0x3a3a3100000000000000000000000000, '2022-01-21 09:24:48', NULL, 1),
 (17, 8, 'summyabubakar03@gmail.com', 0x3a3a3100000000000000000000000000, '2022-01-21 10:05:59', NULL, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -224,118 +138,70 @@ CREATE TABLE `users` (
   `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
 (8, 'Sumayya Abubakar', 'Abuja, Nigeria', 'Abuja', 'female', 'summyabubakar03@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2022-01-21 09:20:57', '2022-01-21 09:24:26');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `appointment`
---
 ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `doctors`
 --
 ALTER TABLE `doctors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `doctorslog`
---
 ALTER TABLE `doctorslog`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `doctorspecilization`
 --
 ALTER TABLE `doctorspecilization`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblcontactus`
---
 ALTER TABLE `tblcontactus`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `userlog`
 --
 ALTER TABLE `userlog`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `appointment`
---
 ALTER TABLE `appointment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `doctorslog`
---
 ALTER TABLE `doctorslog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
---
--- AUTO_INCREMENT for table `doctorspecilization`
 --
 ALTER TABLE `doctorspecilization`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `tblcontactus`
---
 ALTER TABLE `tblcontactus`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `userlog`
 --
 ALTER TABLE `userlog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
